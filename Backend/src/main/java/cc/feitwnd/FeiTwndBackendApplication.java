bc16cc64422ec59602d5749fd1737734532b1a39
FeiTwnd <kiuwoba@qq.com>
2026-01-18 15:42:26 +0800

    初始化

---
+++ Backend/src/main/java/cc/feitwnd/FeiTwndBackendApplication.java
@@ -0,0 +1,13 @@
+package cc.feitwnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiTwndBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeiTwndBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/cc/feitwnd/FeiTwndBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.feitwnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiTwndBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
