74b84b07ec92c084a6bc6cb4d703c64ea339373f
FeiTwnd <kiuwoba@qq.com>
2026-07-14 11:43:31 +0800

    feat: 添加后端城市足迹图片管理接口及相关表

---
+++ Backend/FeiTwnd-server/src/main/java/cc/feitwnd/service/CityFootprintService.java
@@ -0,0 +1,30 @@
+package cc.feitwnd.service;
+
+import cc.feitwnd.dto.CityFootprintDTO;
+import cc.feitwnd.dto.CityFootprintPageQueryDTO;
+import cc.feitwnd.result.PageResult;
+
+import java.util.List;
+
+public interface CityFootprintService {
+
+    /**
+     * 管理端分页查询城市足迹
+     */
+    PageResult pageQuery(CityFootprintPageQueryDTO pageQueryDTO);
+
+    /**
+     * 添加城市足迹
+     */
+    void add(CityFootprintDTO cityFootprintDTO);
+
+    /**
+     * 修改城市足迹
+     */
+    void update(CityFootprintDTO cityFootprintDTO);
+
+    /**
+     * 批量删除城市足迹（同时删除关联图片）
+     */
+    void batchDelete(List<Long> ids);
+}
